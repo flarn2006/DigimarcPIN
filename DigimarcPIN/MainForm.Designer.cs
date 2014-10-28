1aebed64a7fd3043ddd8af7b1dacf752b85fcd0e
flarn2006 <flarn2006@gmail.com>
2014-10-28 19:08:32 -0400

    Real initial commit

---
+++ DigimarcPIN/MainForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace DigimarcPIN
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idBox = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pinBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // idBox
+            // 
+            this.idBox.BackColor = System.Drawing.Color.Black;
+            this.idBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.idBox.Font = new System.Drawing.Font("OCR A Extended", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idBox.ForeColor = System.Drawing.Color.Lime;
+            this.idBox.Location = new System.Drawing.Point(12, 35);
+            this.idBox.Mask = "000000";
+            this.idBox.Name = "idBox";
+            this.idBox.Size = new System.Drawing.Size(180, 46);
+            this.idBox.TabIndex = 0;
+            this.idBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.idBox.TextChanged += new System.EventHandler(this.idBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Digimarc ID";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pinBox
+            // 
+            this.pinBox.BackColor = System.Drawing.Color.Black;
+            this.pinBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pinBox.Font = new System.Drawing.Font("OCR A Extended", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pinBox.ForeColor = System.Drawing.Color.Lime;
+            this.pinBox.Location = new System.Drawing.Point(52, 122);
+            this.pinBox.Name = "pinBox";
+            this.pinBox.ReadOnly = true;
+            this.pinBox.Size = new System.Drawing.Size(100, 57);
+            this.pinBox.TabIndex = 2;
+            this.pinBox.Text = "--";
+            this.pinBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(12, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Your PIN:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(204, 191);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pinBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.idBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Digimarc PIN Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox idBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pinBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DigimarcPIN/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigimarcPIN
+{
+    public partial class MainForm : Form
+    {
+        private int[] key = { 0x1F, 0x05, 0x35, 0x07, 0x2B, 0x2F, 0x0D, 0x02, 0x25, 0x13, 0x03, 0x17, 0x11, 0x29 };
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void idBox_TextChanged(object sender, EventArgs e)
+        {
+            if (idBox.Text.Length == 6) {
+                byte[] stringBytes = Encoding.ASCII.GetBytes(idBox.Text);
+                int n = 0;
+                for (int i = 0; i < idBox.Text.Length; i++) {
+                    byte digit = (byte)(stringBytes[i] - 0x30);
+                    n += digit * key[idBox.Text.Length - 1 - i];
+                }
+                n = n % 89 + 10;
+                pinBox.Text = n.ToString();
+            } else {
+                pinBox.Text = "--";
+            }
+        }
+    }
+}
